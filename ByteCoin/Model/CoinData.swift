0420c15768dfccca786c1bca253272af300af70b
Daulet Ashikbayev <dxulet@gmail.com>
2022-07-01 19:35:17 +0500

    final

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Daulet Ashikbayev on 29.06.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
